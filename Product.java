49e4d3ec64452ca93ebb50cdb7695df498a05ee5
Anshu Kumari <133595936+Anshu10490@users.noreply.github.com>
2025-02-03 21:43:27 +0530

    Solution.java
    Product class with constructor and methods

---
+++ Product.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void display() {
+        System.out.println("Product Details:");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Product ID: ");
+        int id = sc.nextInt();
+        sc.nextLine(); 
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+        sc.close();
+        
+        Product p = new Product(id, name, price);
+        p.display();
+    }
+}
